b4993937153ff1cf6d079f7cb28830145c28e03f
tateshi <yaeo@1010en.com>
2018-04-05 16:05:56 +0900

    Initial Commit

---
+++ iyasakacountapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iyasakacountapp
+//
+//  Created by 八重尾 立志 on 2018/04/05.
+//  Copyright © 2018年 Yaeo Tateshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
