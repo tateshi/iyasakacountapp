e7c65768bd8b1c2c25c38559337fc691595f18a8
tateshi <yaeo@1010en.com>
2018-04-06 05:48:36 +0900

    ２度め

--- iyasakacountapp/ViewController.swift
+++ iyasakacountapp/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("say hi")
+        print("say hi２")
         
     }
 
